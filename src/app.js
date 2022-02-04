3780e251dcdf42cae8ec72377f96e5ff7d50bc39
jorgepardor <jorgeluis.mail@gmail.com>
2022-02-04 12:09:31 +0000

    Ajustes finales al ejercicio

--- src/app.js
+++ src/app.js
@@ -6,27 +6,57 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const cardSuit = [
+  { color: "#A64037", suit: "♥" },
+  { color: "#A64037", suit: "♦" },
+  { color: "#353535", suit: "♣" },
+  { color: "#353535", suit: "♠" }
+];
+
+const cardDeck = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "💂",
+  "👸",
+  "🤴"
+];
+
+// On load solo le pido que ejecute la funcion que genera la carta y que haga que el botón funcione con la clase tryAgain
+
 window.onload = function() {
-  let suit = ["♦", "♥", "♠", "♣"];
-  let card = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  generateCard();
 
-  function randomNumber(arr) {
-    let Number = Math.floor(Math.random() * arr.length);
-    return arr[Number];
-  }
-
-  let cardSuit = randomNumber(suit);
-
-  let cardNumber = randomNumber(card);
-
-  // console.log(cardNumber);
-  // console.log(cardSuit);
-
-  document.querySelector(".suit").innerHTML = cardSuit;
-  document.querySelector(".suit-2").innerHTML = cardSuit;
-  document.querySelector(".number").innerHTML = cardNumber;
-
-  //   const btn = document.querySelector(".tryAgain");
-
-  //   btn.addEventListener("click", randomNumber());
+  document.querySelector(".tryAgain").addEventListener("click", generateCard);
 };
+
+// La funcion genera el numero y el palo de la carta a partir de un random que hay definido mas abajo, en el caso del palo, lo hace en dos partes que conforman un objeto (el color y el símbolo)
+
+function generateCard() {
+  const { color, suit } = randomNumber(cardSuit);
+  const cardNumber = randomNumber(cardDeck);
+
+  document.querySelectorAll("#suit").forEach(element => {
+    element.style.color = color;
+    element.innerHTML = suit;
+  });
+
+  document.querySelectorAll("#suit-2").forEach(element => {
+    element.style.color = color;
+    element.innerHTML = suit;
+  });
+
+  document.querySelector(".number").innerHTML = cardNumber;
+}
+
+function randomNumber(arr) {
+  let Number = Math.floor(Math.random() * arr.length);
+  return arr[Number];
+}
